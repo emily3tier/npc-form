6c4eed50421aa7dcb621a4dbe75e987228e7ae66
emily3tier <emily@3tierbeverages.com>
2026-04-21 17:39:04 -0500

    Update upload_file.js

--- api/upload_file.js
+++ api/upload_file.js
@@ -1,73 +1,11 @@
 const https = require('https');
 
-const CLIENT_ID = 'd6dd88e1-a49e-4350-8339-f0f42c4b3b2e';
-const TENANT_ID = '667afa82-1126-4a78-8f76-0918c7f2a845';
-const BASE_FOLDER = 'UPC Submissions Automated';
-
 function getRawBody(req) {
   return new Promise((resolve, reject) => {
     const chunks = [];
     req.on('data', chunk => chunks.push(chunk));
     req.on('end', () => resolve(Buffer.concat(chunks)));
     req.on('error', reject);
-  });
-}
-
-function enc(s) { return encodeURIComponent(s); }
-
-function uploadViaSession(token, fullPath, data) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const sessionBody = JSON.stringify({ item: { '@microsoft.graph.conflictBehavior': 'replace' } });
-      const sessionResp = await new Promise((res, rej) => {
-        const r = https.request({
-          hostname: 'graph.microsoft.com',
-          path: `/v1.0/me/drive/root:/${fullPath}:/createUploadSession`,
-          method: 'POST',
-          headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(sessionBody) },
-        }, (response) => {
-          let d = '';
-          response.on('data', c => d += c);
-          response.on('end', () => response.statusCode >= 400 ? rej(new Error(`Session error ${response.statusCode}: ${d}`)) : res(JSON.parse(d)));
-        });
-        r.on('error', rej);
-        r.write(sessionBody);
-        r.end();
-      });
-
-      const uploadUrl = new URL(sessionResp.uploadUrl);
-      const req2 = https.request({
-        hostname: uploadUrl.hostname,
-        path: uploadUrl.pathname + uploadUrl.search,
-        method: 'PUT',
-        headers: { 'Content-Length': data.length, 'Content-Range': `bytes 0-${data.length - 1}/${data.length}` },
-      }, (response) => {
-        let d = '';
-        response.on('data', c => d += c);
-        response.on('end', () => response.statusCode >= 400 ? reject(new Error(`Upload error ${response.statusCode}: ${d}`)) : resolve(d));
-      });
-      req2.on('error', reject);
-      req2.write(data);
-      req2.end();
-    } catch (e) { reject(e); }
-  });
-}
-
-function uploadDirect(token, fullPath, data) {
-  return new Promise((resolve, reject) => {
-    const req = https.request({
-      hostname: 'graph.microsoft.com',
-      path: `/v1.0/me/drive/root:/${fullPath}:/content`,
-      method: 'PUT',
-      headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/octet-stream', 'Content-Length': data.length },
-    }, (res) => {
-      let d = '';
-      res.on('data', c => d += c);
-      res.on('end', () => res.statusCode >= 400 ? reject(new Error(`Upload error ${res.statusCode}: ${d}`)) : resolve(d));
-    });
-    req.on('error', reject);
-    req.write(data);
-    req.end();
   });
 }
 
@@ -79,16 +17,32 @@ module.exports = async (req, res) => {
 
   try {
     const rawBody = await getRawBody(req);
-    const { token, folderName, fileName, data } = JSON.parse(rawBody.toString('utf8'));
+    const { uploadUrl, data, start, end, total } = JSON.parse(rawBody.toString('utf8'));
 
     const buf = Buffer.from(data, 'base64');
-    const fullPath = `${BASE_FOLDER}/${folderName}/${fileName}`.split('/').map(enc).join('/');
+    const urlObj = new URL(uploadUrl);
 
-    if (buf.length > 4 * 1024 * 1024) {
-      await uploadViaSession(token, fullPath, buf);
-    } else {
-      await uploadDirect(token, fullPath, buf);
-    }
+    await new Promise((resolve, reject) => {
+      const r = https.request({
+        hostname: urlObj.hostname,
+        path: urlObj.pathname + urlObj.search,
+        method: 'PUT',
+        headers: {
+          'Content-Length': buf.length,
+          'Content-Range': `bytes ${start}-${end}/${total}`,
+        },
+      }, (response) => {
+        let d = '';
+        response.on('data', c => d += c);
+        response.on('end', () => {
+          if (response.statusCode >= 400) reject(new Error(`Upload failed ${response.statusCode}: ${d}`));
+          else resolve(d);
+        });
+      });
+      r.on('error', reject);
+      r.write(buf);
+      r.end();
+    });
 
     res.status(200).json({ ok: true });
   } catch (e) {
